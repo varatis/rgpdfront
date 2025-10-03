d7ec5b243ae56b0587f97495d08b0053b90366ca
MEGDICH Rihab <rihab.megdich@groupe-creative.fr>
2025-10-03 14:28:07 +0200

    feat(auth): finalisation page de connexion

--- src/app/features/auth/auth-module.ts
+++ src/app/features/auth/auth-module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing-module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Login } from './pages/login/login';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    // ...existing code...
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    ReactiveFormsModule,
+    AuthRoutingModule,
+    Login
   ]
 })
 export class AuthModule { }
--- src/app/features/auth/auth-routing-module.ts
+++ src/app/features/auth/auth-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Login } from './pages/login/login';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Login
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
